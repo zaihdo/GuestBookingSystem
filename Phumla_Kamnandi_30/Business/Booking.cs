530c514bd571dc01f67c859ba1d98d711dc680d9
tytac116 <75938229+tytac116@users.noreply.github.com>
2021-10-17 11:31:21 +0200

    First File Upload

---
+++ Phumla_Kamnandi_30/Business/Booking.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phumla_Kamnandi_30.Business_layer
+{
+    class Booking
+    {
+        #region data members
+        private string bookingID;
+        private int numRooms;
+        private DateTime checkIn;
+        private DateTime checkOut;
+        private int totalCharge;
+        private bool depositPaid;
+        #endregion
+
+        #region properties
+        public string getBookingID
+        {
+            get { return bookingID;  }
+        }
+
+        public int getNumRooms
+        {
+            get { return numRooms;  }
+            set { numRooms = value; }
+        }
+
+        public DateTime getCheckIn
+        {
+            get { return checkIn ;  }
+            set { checkIn = value;  }
+        }
+
+        public DateTime getCheckOut
+        {
+            get { return checkOut; }
+            set { checkOut = value; }
+        }
+
+        public int getTotalCharge
+        {
+            get { return totalCharge; }
+            set { totalCharge = value; }
+        }
+
+        public bool getDepositPaid
+        {
+            get { return depositPaid; }
+            set { depositPaid = value; }
+        }
+        #endregion
+    }
+}
---
+++ Phumla_Kamnandi_30/Business/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Phumla_Kamnandi_30.Business_layer
+{
+
+    #region data members
+
+    private string empID;
+    private Role roleType;
+
+    #endregion
+
+    class Employee
+    {
+    }
+}
